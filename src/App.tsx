23fe1c31abff1afc0debd30cd18b6308b51a3d30
jo-duchan <jo_duchan@icloud.com>
2023-01-27 14:30:18 +0900

    Demo 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,22 @@ function App() {
   return (
     <Container>
       <Title>React Custom Observer Demo</Title>
-      <Body>
+      <Description>
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen{" "}
+      </Description>
+      <Body>
+        {[...Array(6)].map((x, i) => (
+          <BoxOuter key={i}>
+            <Label>Threshold {(i * 0.2).toFixed(1)}</Label>
+            <Observer threshold={i * 0.2}>
+              <Box />
+            </Observer>
+          </BoxOuter>
+        ))}
       </Body>
-      {[...Array(6)].map((x, i) => (
-        <BoxOuter key={i}>
-          {(i * 0.2).toFixed(1)}
-          <Observer threshold={i * 0.2}>
-            <Box />
-          </Observer>
-        </BoxOuter>
-      ))}
     </Container>
   );
 }
@@ -29,18 +31,33 @@ export default App;
 
 const Container = styled.div`
   width: 100%;
-  padding: 5vw;
-  height: 200vh;
+  padding: 7vw;
+  height: auto;
   box-sizing: border-box;
 `;
 
 const Title = styled.h1`
-  /* margin-top: 50vh; */
+  color: #101010;
+`;
+
+const Description = styled.p`
+  width: 50%;
+  font-size: 16px;
+  line-height: 1.5em;
+  color: #303030;
 `;
 
 const Body = styled.div`
+  margin-top: 80vh;
+  margin-bottom: 10vh;
+`;
+
+const Label = styled.span`
+  display: inline-block;
+  margin-bottom: 50px;
   font-size: 20px;
-  line-height: 1.5em;
+  font-weight: 600;
+  color: #101010;
 `;
 
 const BoxOuter = styled.div`
